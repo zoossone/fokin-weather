22269ef3b6ee0ce7061032f55621f3f186ad3195
Chu Sangwon <zoossone@gmail.com>
2021-07-27 21:37:31 +0900

    about component and flex :)

--- App.js
+++ App.js
@@ -5,20 +5,25 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Hello!!</Text>
+      <View style={styles.yellowView}><Text>Hello</Text></View>
+      <View style={styles.blueView}><Text>Hello</Text></View>
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  //부모가 하나의 flex를 가짐
   container: {
     flex: 1,
-    backgroundColor: 'red',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
-  text: {
-    color: 'white'
+  //자식들도 본인이 가지고 싶은 영역의 크기만큼 flex로 영역을 가짐
+  yellowView: {
+    flex: 1,
+    backgroundColor: "yellow"
+  },
+  blueView: {
+    flex: 3,
+    backgroundColor: "blue"
   }
 });
