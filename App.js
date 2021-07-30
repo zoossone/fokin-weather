14cb29a492116cdeb6af90682b2a88da8ea5c793
Chu Sangwon <zoossone@gmail.com>
2021-07-30 16:04:01 +0900

    get temp from API to state :)

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { Alert } from 'react-native';
 import Loading from './Loading';
 import * as Location from "expo-location";
 import axios from "axios";
+import Weather from './Weather';
 
 const API_KEY = "eb51dace332d7014831ef1553c9953ac";
 
@@ -14,9 +15,10 @@ export default class extends React.Component {
   }
   getWeather = async(latitude, longitude) => {
     const { data } = await axios(
-      `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`
+      `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`
       );
       console.log(data);
+      this.setState({isLoading: false, temp: data.main.temp});
   }
   getLocation = async () => {
     try {
@@ -25,7 +27,6 @@ export default class extends React.Component {
         coords: {latitude, longitude} 
       } = await Location.getCurrentPositionAsync();
       this.getWeather(latitude, longitude);
-      this.setState({isLoading: false});
       //Send To API and get weather!
     } catch (error) {
       Alert.alert("Can't find you", "So sad");
@@ -35,7 +36,7 @@ export default class extends React.Component {
     this.getLocation();
   }
   render() {
-    const {isLoading} = this.state;
-    return isLoading ? <Loading /> : null;
+    const {isLoading, temp} = this.state;
+    return isLoading ? <Loading /> : <Weather temp={Math.round(temp)}/>;
   }
 }
---
+++ Weather.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { StyleSheet, View, Text } from "react-native";
+import PropTypes from "prop-types";
+
+export default function Weather({temp}) {
+    return <View style={styles.container}>
+        <Text>{temp}</Text>
+        </View>
+}
+
+Weather.PropTypes = {
+    temp: PropTypes.number.isRequired
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center"
+    }
+})
